2e4881aa6db5180b79f5d8616a955b3bf03ec243
srichatala <sri2vas@gmail.com>
2016-11-29 22:31:17 -0500

    initial files added to configure go web app

---
+++ src/main/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func main() {
+    http.HandleFunc("/",someFunc)
+    http.HandleFunc("/about",aboutFunc)
+    http.ListenAndServe(":8088",nil)
+}
+
+func someFunc(w http.ResponseWriter, req *http.Request){
+    w.Write([]byte("Hello world, welcome to http"))
+}
+
+func aboutFunc(w http.ResponseWriter, req *http.Request){
+    w.Write([]byte("Welcome to about page"))
+}
